7bcbaf56d0559815a0fda12332d96a2415de5ea4
李泽昆 <844327426@qq.com>
2017-03-13 11:09:42 +0800

    s

--- tireKingDemo/tireKingDemo/Home/C/HomeViewController.swift
+++ tireKingDemo/tireKingDemo/Home/C/HomeViewController.swift
@@ -12,18 +12,12 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD
+
         // 背景颜色
         view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
     }
-=======
-        print(22)
-        
-    }
 
-    
-    
->>>>>>> 70189808b0c63fd7d6c97748788aab27d8f0da33
+
 
 
 }
--- tireKingDemo/tireKingDemo/Root/C/RootViewController.swift
+++ tireKingDemo/tireKingDemo/Root/C/RootViewController.swift
@@ -13,7 +13,7 @@ class RootViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-<<<<<<< HEAD
+
         // 创建一个tabbar的控制器
         creatSubViewController()
     }
@@ -32,7 +32,7 @@ class RootViewController: UITabBarController {
         storeVC.tabBarItem = storeItem
         
         // 创建发现
-        let findVC = StoreViewController()
+        let findVC = FindViewController()
         let findItem: UITabBarItem = UITabBarItem (title: "发现", image: nil, selectedImage: nil)
         findVC.tabBarItem = findItem
         
@@ -44,8 +44,6 @@ class RootViewController: UITabBarController {
         // 添加自控制器
         let tabArray = [homeVC, storeVC, findVC, meVC]
         self.viewControllers = tabArray
-        
-=======
->>>>>>> 70189808b0c63fd7d6c97748788aab27d8f0da33
+
     }
 }
