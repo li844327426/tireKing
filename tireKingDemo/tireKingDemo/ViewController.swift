2c905e3e432a4219fbdb4cc91a4288a7b77eefe2
杨 <yang@yangdeMacBook-Pro.local>
2017-03-13 09:53:25 +0800

    修改成功

--- tireKingDemo/tireKingDemo/ViewController.swift
+++ tireKingDemo/tireKingDemo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello word")
+        print("Hello")
     }
 
     override func didReceiveMemoryWarning() {
