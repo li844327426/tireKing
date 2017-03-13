70189808b0c63fd7d6c97748788aab27d8f0da33
李泽昆 <844327426@qq.com>
2017-03-13 10:46:02 +0800

    FInd Bug

---
+++ tireKingDemo/tireKingDemo/Find/C/FindViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FindViewController.swift
+//  tireKingDemo
+//
+//  Created by 李泽昆 on 17/3/13.
+//  Copyright © 2017年 Frank-Lee. All rights reserved.
+//
+
+import UIKit
+
+class FindViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
