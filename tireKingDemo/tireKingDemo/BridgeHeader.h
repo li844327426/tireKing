b969e43f76bd373c1ec6dfce636ea6d7487ae7af
李泽昆 <844327426@qq.com>
2017-03-13 13:25:17 +0800

    Home

---
+++ tireKingDemo/tireKingDemo/BridgeHeader.h
@@ -0,0 +1,19 @@
+//
+//  BridgeHeader.h
+//  tireKingDemo
+//
+//  Created by 李泽昆 on 17/3/13.
+//  Copyright © 2017年 Frank-Lee. All rights reserved.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+
+#endif /* BridgeHeader_h */
+
+
+
+
+
+#import "SDCycleScrollView.h"
--- tireKingDemo/tireKingDemo/Home/C/HomeViewController.swift
+++ tireKingDemo/tireKingDemo/Home/C/HomeViewController.swift
@@ -8,16 +8,61 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, SDCycleScrollViewDelegate, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // 背景颜色
         view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
+        
+        
+        
+        setTableView()
+        
     }
 
-
-
+    func setTableView() {
+        let tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        let bannerImages = [UIImage.init(named: "11"),UIImage.init(named: "11")]
+        print(bannerImages)
+        let bannerView = SDCycleScrollView(frame: CGRect.init(x: 0, y: 0, width: view.bounds.size.width, height: 160), imageNamesGroup: bannerImages)
+        
+        bannerView?.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        bannerView?.delegate = self
+        tableView.tableHeaderView = bannerView
+        view.addSubview(tableView)
+    }
+    
+    //MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "22")
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    //MARK: UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 100
+        case 1:
+            return 80
+        default:
+            return 200
+        }
+    }
 
 }
