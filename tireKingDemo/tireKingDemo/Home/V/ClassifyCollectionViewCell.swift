07ff948bc0bb95e843c3f1c52230cc8c3624df7e
李泽昆 <844327426@qq.com>
2017-03-13 19:09:59 +0800

    第一天

---
+++ tireKingDemo/tireKingDemo/Home/V/ClassifyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ClassifyCollectionViewCell.swift
+//  tireKingDemo
+//
+//  Created by 李泽昆 on 17/3/13.
+//  Copyright © 2017年 Frank-Lee. All rights reserved.
+//
+
+import UIKit
+
+class ClassifyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
